9879f3979a5ec6f9ccd3b596c25030686f9c8507
Ian-Stentz <ianstentz25@gmail.com>
2024-10-23 11:46:57 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,19 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+const newline = document.createElement("div");
+app.append(newline);
+
+const canvas = document.createElement("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 256;
+canvas.height = 256;
+
+if(ctx) {
+    ctx.fillStyle = "white";
+    ctx.fillRect(0,0,256,256);
+}
+
+app.append(canvas);
