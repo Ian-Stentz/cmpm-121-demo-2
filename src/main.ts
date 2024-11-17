ab86da1826fa0624624b960281469cf25ee641ad
Ian-Stentz <ianstentz25@gmail.com>
2024-11-16 19:26:42 -0800

    Step 5 Complete

--- src/main.ts
+++ src/main.ts
@@ -12,10 +12,10 @@ enum ToolType {
 }
 
 interface SplineTool {
-    pointData : Point[] | Point;
-    display(ctx : CanvasRenderingContext2D) : void;
-    drag?(x : number, y : number) : void;
-    //tool : ToolType;    
+    pointData : Point[];
+    display(ctx : CanvasRenderingContext2D, pointData : Point[], tool : ToolType) : void;
+    drag?(spline : SplineTool, point : Point) : void;
+    tool : ToolType;    
 }
 
 let penDown = false;
@@ -53,19 +53,34 @@ resetCanvas();
 app.append(canvas);
 
 //TODO: STEP 5 Spline Drag Event
-function addDisplayListPoint(x : number, y : number) {
-    let l = displayList.length;
-    if(l > 0) {
-        let thisPoint : Point = {x: x,y: y};
-        displayList[l-1].push(thisPoint);
-    }
+// function addDisplayListPoint(x : number, y : number) {
+//     const l = displayList.length;
+//     if(l > 0) {
+//         const thisPoint : Point = {x: x,y: y};
+//         displayList[l-1].pointData.push(thisPoint);
+//     }
+// }
+
+function getCurSpline() : SplineTool{
+    const l = displayList.length;
+    return displayList[l-1];
 }
 
-function drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+function drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number, thickness : ToolType) {
     if(context) {
         context.beginPath();
         context.strokeStyle = "black";
-        context.lineWidth = 2;
+        switch (thickness) {
+            case ToolType.Thick:
+                context.lineWidth = 3;
+                break;
+            case ToolType.Thin:
+                context.lineWidth = 1;
+                break;
+            default:
+                context.lineWidth = 2;
+                break;
+        }
         context.moveTo(x1, y1);
         context.lineTo(x2, y2);
         context.stroke();
@@ -74,45 +89,60 @@ function drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, 
 }
 
 //TODO: STEP 5 display event
-function drawCanvas() {
-    let newline = true;
-    let lastPoint : Point = {x: 0,y: 0};
-    for(let stroke of displayList) {
-        newline = true;
-        for(let thisPoint of stroke) {
-            if(!newline && ctx) {
-                drawLine(ctx, lastPoint.x, lastPoint.y, thisPoint.x, thisPoint.y);
-            } else {
-                newline = false;
-            }
-            lastPoint = {x: thisPoint.x, y: thisPoint.y};
+function drawCanvas(context : CanvasRenderingContext2D | null) {
+    for(const stroke of displayList) {
+        if(context) {
+            stroke.display(context, stroke.pointData, stroke.tool);
         }
     }
 }
 
-canvas.addEventListener("drawing-changed", (e) => {
+function drawPen(context : CanvasRenderingContext2D, pointData : Point[], tool : ToolType) {
+    let lastPoint : Point | null = null;
+    for(const thisPoint of pointData) {
+        if(context && lastPoint) {
+            drawLine(context, lastPoint.x, lastPoint.y, thisPoint.x, thisPoint.y, tool);
+        }
+        lastPoint = {x: thisPoint.x, y: thisPoint.y};
+    }
+}
+
+function dragSpline(spline : SplineTool, point : Point) {
+    spline.pointData.push(point);
+}
+
+canvas.addEventListener("drawing-changed", () => {
     resetCanvas();
-    drawCanvas();
+    drawCanvas(ctx);
 });
 
 //TODO : STEP 5 Create spline
 canvas.addEventListener("mousedown", (e) => {
     penDown = true;
-    displayList.push([]);
-    addDisplayListPoint(e.offsetX, e.offsetY);
+    displayList.push({pointData : [], tool : ToolType.Thin, display : drawPen, drag : dragSpline});
+    const curSpline : SplineTool = getCurSpline();
+    if(curSpline.drag) {
+        curSpline.drag(curSpline, {x : e.offsetX, y : e.offsetY});
+    }
 })
 
 canvas.addEventListener("mousemove", (e) => {
     if(penDown) {
         //drawLine(ctx, mouseX, mouseY, e.offsetX, e.offsetY);
-        addDisplayListPoint(e.offsetX, e.offsetY);
+        const curSpline : SplineTool = getCurSpline();
+        if(curSpline.drag) {
+            curSpline.drag(curSpline, {x : e.offsetX, y : e.offsetY});
+        }
         canvas.dispatchEvent(drawingChanged);
     }
 })
 
 canvas.addEventListener("mouseup", (e) => {
     if(penDown) {
-        addDisplayListPoint(e.offsetX, e.offsetY);
+        const curSpline : SplineTool = getCurSpline();
+        if(curSpline.drag) {
+            curSpline.drag(curSpline, {x : e.offsetX, y : e.offsetY});
+        }
         canvas.dispatchEvent(drawingChanged);
         penDown = false;
     }
@@ -124,7 +154,7 @@ function resetDisplayInfo() {
 }
 
 function undo() {
-    let newestStroke = displayList.pop();
+    const newestStroke = displayList.pop();
     if(newestStroke) {
         redoStack.push(newestStroke);
     }
@@ -132,7 +162,7 @@ function undo() {
 }
 
 function redo() {
-    let redo = redoStack.pop();
+    const redo = redoStack.pop();
     if(redo) {
         displayList.push(redo);
     }
